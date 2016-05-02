d104a7a3ba14fbf5b0f1e9ac8d6dd12d429b270d
dianeschulze <dschulze@dianes-air-2.devices.brown.edu>
2016-05-02 13:17:19 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+
+var port = Number(process.env.PORT || 3000);
+
+var request = require('request');
+var engines = require('consolidate');
+var express = require('express'),
+    app = express();
+var session = require('express-session');
+var path = require('path');
+
+
+app.get('/', function(req, res) {
+    res.send('Hi I am an email server');
+});
+
+
+app.get('/helloworld', function(req, res) {
+    console.log('got hello world request');
+    res.end();
+});
+
+// on receipt of new email
+app.post('/email', function(req, res){
+    console.log('got email')
+    var sender = req.body.FromAddress
+    var subject = req.body.Subject
+    var body = req.body.BodyPlain
+    var time = req.body.ReceivedAt
+    console.log(sender, subject, body, time)
+    res.end()
+});
+
+
+app.listen(port);
+
